919b5900a46439c6b49305b4c323d3bdffefaea4
Jerzy Balwinski <Jerzy.Balwinski@partner.bmw.de>
2024-09-19 14:02:39 +0200

    Lab 1

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -1,5 +1,6 @@
 package com.awstraining.backend.business.notifyme;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,13 +8,14 @@ public class NotifyMeService {
 
     // TODO: lab1
     //  1. Inject MessageSender.
+    private final MessageSender messageSender;
     // TODO lab2
     //  1. Inject Translator
     // TODO lab3
     //  1. Inject sentiment detector
-//    @Autowired
-    public NotifyMeService() {
-
+    @Autowired
+    public NotifyMeService(MessageSender messageSender) {
+        this.messageSender = messageSender;
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
@@ -21,6 +23,8 @@ public class NotifyMeService {
         // TODO: lab1
         //  1. Send text using sender.
         //  2. Return sent message.
+        messageSender.send(notifyMe.text());
+
         // TODO: lab2
         //  1. Translate text from using translator.
         //  2. Change sending of text to "translated text" and return it.
--- aws-infrastructure/terraform/policies/ecs-task-role-policy.tpl
+++ aws-infrastructure/terraform/policies/ecs-task-role-policy.tpl
@@ -32,6 +32,20 @@ ${jsonencode(
         "arn:aws:ssm:${region}:${account_id}:parameter/config/application*",
         "arn:aws:ssm:${region}:${account_id}:parameter/config/backend*"
       ]
+    },
+    {
+    "Effect": "Allow",
+    "Action": "sns:Publish",
+    "Resource": "*"
+    },
+    {
+    "Effect": "Allow",
+    "Action": "translate:TranslateText",
+    "Resource": "*"
+    },
+    "Effect": "Allow",
+    "Action": "comprehend:DetectSentiment",
+    "Resource": "*"
     }
   ]
 }
