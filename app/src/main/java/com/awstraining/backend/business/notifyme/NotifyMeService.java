14225c52994733a0f6c7dc88d36317ba3c13a09a
Jerzy Balwinski <Jerzy.Balwinski@partner.bmw.de>
2024-09-19 14:46:02 +0200

    Lab 2

--- app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
+++ app/src/main/java/com/awstraining/backend/business/notifyme/NotifyMeService.java
@@ -9,13 +9,17 @@ public class NotifyMeService {
     // TODO: lab1
     //  1. Inject MessageSender.
     private final MessageSender messageSender;
+
     // TODO lab2
     //  1. Inject Translator
+    private final Translator translator;
+
     // TODO lab3
     //  1. Inject sentiment detector
     @Autowired
-    public NotifyMeService(MessageSender messageSender) {
+    public NotifyMeService(MessageSender messageSender, Translator translator) {
         this.messageSender = messageSender;
+        this.translator = translator;
     }
     
     public String notifyMe(NotifyMeDO notifyMe) {
@@ -28,6 +32,9 @@ public class NotifyMeService {
         // TODO: lab2
         //  1. Translate text from using translator.
         //  2. Change sending of text to "translated text" and return it.
+        String translated = translator.translate(notifyMe);
+        messageSender.send(translated);
+
         // TODO: lab3
         //  1. Detect sentiment of translated message.
         //  2. Change sending of text to "setiment: translated text" and return it.
